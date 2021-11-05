2c5b5078d82dfce202ace1677a0d346819cab8f7
ThePeachster <jasonpeacherton@gmail.com>
2021-11-05 15:40:33 -0500

    Commit before i mess some things up

--- Javascript/galleryjs.js
+++ Javascript/galleryjs.js
@@ -1,11 +1,15 @@
 'use strict'
 
 function openmodal() {
-  document.getElementById('myModal').style.display = "block";
+  document.getElementById('myModal').style.display = "inline-block";
+  document.getElementById('close').style.display = "inline";
+  document.getElementById('content').style.display = "block";
 }
 
 function closemodal() {
     document.getElementById('myModal').style.display = "none";
+    document.getElementById('close').style.display = "none";
+    document.getElementById('content').style.display = "none";
   }
 
 function openphoto(n) {
